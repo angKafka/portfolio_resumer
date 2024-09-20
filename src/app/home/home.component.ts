da8f8aa932efa74be17264c36a583ef277c7fb8a
mean-stack-app <ngtrainerdeveloper@gmail.com>
2024-09-20 12:42:20 +0530

    Type writter effect on greetings home-page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'] // Fix here
 })
-export class HomeComponent implements OnInit{
-  greetingMessage: string = ''; // This will hold the dynamic greeting message
+export class HomeComponent implements OnInit {
+  greetingMessage: string = '';
+  displayedMessage: string = '';
+  isTyping: boolean = false;
 
-  // This function sets the greeting based on the time
+  ngOnInit(): void {
+    this.setGreeting(); 
+    this.startTyping();
+  }
+
+  startTyping() {
+    this.isTyping = true; 
+    this.displayedMessage = ''; // Clear displayed message first
+    let i = 0;
+
+    const type = () => {
+      if (i < this.greetingMessage.length) {
+        this.displayedMessage += this.greetingMessage.charAt(i);
+        i++;
+        setTimeout(type, 100); // Adjust speed here
+      } else {
+        this.isTyping = false; 
+      }
+    };
+
+    type();
+  }
+
   setGreeting() {
     const currentHour = new Date().getHours(); // Get the current hour (0-23)
 
     if (currentHour >= 0 && currentHour < 12) {
       this.greetingMessage = "Good Morning";
-    } else if (currentHour >= 12 && currentHour <= 15.5) {
+    } else if (currentHour >= 12 && currentHour < 16) {
       this.greetingMessage = "Good Afternoon";
-    } else if (currentHour > 15.5 && currentHour <= 18.5) {
+    } else if (currentHour >= 16 && currentHour < 19) {
       this.greetingMessage = "Good Evening";
-    } else if (currentHour > 18.5 && currentHour < 24) {
+    } else {
       this.greetingMessage = "Good Night";
     }
-  }
-
-  
-  ngOnInit(): void {
-    this.setGreeting(); 
   }
 }
